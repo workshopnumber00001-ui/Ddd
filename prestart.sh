c1b29b6ab6d4298193490c24bfc4e47768766aff
workshopnumber00001-ui <workshopnumber00001@gmail.com>
2026-07-07 09:57:08 +0530

    Update prestart.sh

--- prestart.sh
+++ prestart.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# यह स्क्रिप्ट सभी जरूरी __init__.py फाइलें बना देगी
-echo "Creating __init__.py files..."
+echo "🚀 Ensuring package directories exist..."
+mkdir -p modules master constant
 touch modules/__init__.py
 touch master/__init__.py
 touch constant/__init__.py
-echo "✅ All __init__.py files created."
+echo "✅ Folders and __init__.py files successfully created!"
+ls -la modules
