953e1a03673770501d1530682388f7785db7b5d4
workshopnumber00001-ui <workshopnumber00001@gmail.com>
2026-07-07 09:48:44 +0530

    Create prestart.sh

---
+++ prestart.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# यह स्क्रिप्ट सभी जरूरी __init__.py फाइलें बना देगी
+echo "Creating __init__.py files..."
+touch modules/__init__.py
+touch master/__init__.py
+touch constant/__init__.py
+echo "✅ All __init__.py files created."
