b87ccdc274c3fa1a0874c43e004b5d78d77e4413
workshopnumber00001-ui <workshopnumber00001@gmail.com>
2026-07-07 10:06:04 +0530

    Update prestart.sh

--- prestart.sh
+++ prestart.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-echo "🚀 Ensuring package directories exist..."
 mkdir -p modules master constant
 touch modules/__init__.py
 touch master/__init__.py
 touch constant/__init__.py
-echo "✅ Folders and __init__.py files successfully created!"
-ls -la modules
+echo "✅ All __init__.py files created."
